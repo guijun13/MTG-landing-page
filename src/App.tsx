a530ba51a61f00e994e1133069a11e3e6c5ec4b7
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-14 10:54:39 -0300

    fix: style header nav items

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ export default function App() {
   return (
     <>
       <div className="p-4 bg-gradient-to-r from-orange-400 to-pink-600">
-        <header className="flex lg:justify-between">
+        <header className="flex justify-between">
           <a href={data.logo.url}>
             <img src={data.logo.img} alt={data.logo.alt} />
           </a>
           <nav className="flex items-center gap-x-4">
-            {data.navbar.map((item) => {
-              return (
-                <div key={item.id}>
-                  <a className="text-slate-50" href={item.url}>
-                    {item.name}
-                  </a>
-                </div>
-              );
-            })}
+            <div className="flex flex-col md:flex-row md:gap-x-4">
+              {data.navbar.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <a className="text-slate-50" href={item.url}>
+                      {item.name}
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
             <button className="p-2 border border-slate-200 rounded-full">Login</button>
             <button className="p-2 rounded-full bg-indigo-500 text-slate-50">Sign up free</button>
           </nav>
