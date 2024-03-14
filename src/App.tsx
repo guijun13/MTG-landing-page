36d0bf17baabf2952c6b593a331a4838fbca8e35
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-14 09:32:35 -0300

    feat: create main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,70 @@
 export default function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <header>
+        <button>Mind the Graph logo</button>
+        <nav>
+          <a href="">Templates</a>
+          <a href="">Pricing</a>
+          <a href="">Blog</a>
+          <a href="">Jobs</a>
+          <button>Login</button>
+          <button>Sign up free</button>
+        </nav>
+      </header>
+      <body>
+        <section>
+          <div>
+            <p>
+              Discover all possibilities of using this powerfull tool and create science figures,
+              posters and infographics.
+            </p>
+            <button>Start creating now</button>
+          </div>
+          <img src="" alt="" />
+        </section>
+        <section>
+          <h1>Visualize what Mind the Graph can do for you</h1>
+          <div>
+            <div>
+              <img src="" alt="" />
+              <title>Graphical Abstracts and Infographics</title>
+              <p>Improve your papers impact and visibility through quality visual communication</p>
+              <button>&gt; Learn more</button>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <title>Scientific Poster in a few clicks</title>
+              <p>Add visual impact to your posters with scientific illustrations and graphics</p>
+              <button>&gt; Learn more</button>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <title>Make better Slide Presentation</title>
+              <p>
+                Transform your lectures, meetings and classes into high-impact visual experiences
+              </p>
+              <button>&gt; Learn more</button>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h1>Create effective science figures in minutes</h1>
+          <h3>
+            We built Mind the Graph for simplicity. The platform is easy to use and just about
+            anybody can use it to create great infographics and presentations - from beginners to
+            professionals, individuals to groups and small labs to large organisations
+          </h3>
+          <div>
+            <div>
+              <img src="" alt="" />
+              <title></title>
+              <p></p>
+            </div>
+          </div>
+          <button>Explore Mind the graph</button>
+        </section>
+      </body>
+    </>
+  );
 }
